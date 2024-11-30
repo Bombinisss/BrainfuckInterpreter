09f8ad8bb571b0074a405a397598df1a0bff0979
Bombini <antekbom@gmail.com>
2024-11-30 18:00:35 +0100

    Added boxes that represent memory, started writing thread

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,9 @@
 use egui::{Color32, Painter, Pos2, Rect, Stroke, Vec2};
 use egui_file_dialog::FileDialog;
+use std::ops::Deref;
+use std::sync::{Arc, Mutex};
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 /// We derive Deserialize/Serialize, so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -11,17 +13,21 @@ pub struct BrainfuckInterpreterInterface {
     #[serde(skip)]
     file_dialog: FileDialog,
     #[serde(skip)]
-    letter_index: usize,
+    letter_index: Arc<Mutex<i64>>,
     #[serde(skip)]
-    frame_count: usize,
+    last_update_time: Instant,
     #[serde(skip)]
-    last_update_time: std::time::Instant,
+    input_text: Arc<Mutex<String>>,
     #[serde(skip)]
-    input_text: String,
+    input_brainfuck: Arc<Mutex<String>>,
     #[serde(skip)]
-    input_brainfuck: String,
+    output: Arc<Mutex<String>>,
     #[serde(skip)]
-    output: String,
+    data: Arc<Mutex<Vec<i32>>>,
+    #[serde(skip)]
+    timer_running: Arc<Mutex<bool>>,
+    #[serde(skip)]
+    timer_thread_handle: Option<thread::JoinHandle<()>>,
 }
 
 impl Default for BrainfuckInterpreterInterface {
@@ -34,12 +40,14 @@ impl Default for BrainfuckInterpreterInterface {
                 .max_size([595.0, 375.0])
                 .resizable(false)
                 .movable(false),
-            letter_index: 0,
-            frame_count: 0,
+            letter_index: Arc::new(Mutex::new(-1)),
             last_update_time: std::time::Instant::now(),
-            input_text: "".to_string(),
-            input_brainfuck: "".to_string(),
-            output: "".to_string(),
+            input_text: Arc::new(Mutex::new("".to_string())),
+            input_brainfuck: Arc::new(Mutex::new("".to_string())),
+            output: Arc::new(Mutex::new("".to_string())),
+            data: Arc::new(Mutex::new(vec![0; 32])),
+            timer_running: Arc::new(Mutex::new(false)),
+            timer_thread_handle: None,
         }
     }
 }
@@ -50,24 +58,54 @@ impl BrainfuckInterpreterInterface {
         // This is also where you can customize the look and feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
 
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
         if let Some(storage) = cc.storage {
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         }
 
         Default::default()
     }
+    pub fn start_interpreter(&mut self) {
+        // Start the timer on a separate thread if it is not already running
+        let timer_running = Arc::clone(&self.timer_running);
+        let data_arc = Arc::clone(&self.data);
+        let letter_index_arc = Arc::clone(&self.letter_index);
+        let input_brainfuck = Arc::clone(&self.input_brainfuck);
+        let input_text = Arc::clone(&self.input_text);
+        let output_brainfuck = Arc::clone(&self.output);
+        
+        if *timer_running.lock().unwrap() {
+            return; // Timer is already running
+        }
+
+        *timer_running.lock().unwrap() = true;
+
+        // Spawn a thread for the timer
+        self.timer_thread_handle = Some(thread::spawn(move || {
+            while *timer_running.lock().unwrap() {
+                let mut letter_index = letter_index_arc.lock().unwrap().clone();
+                let data_length = data_arc.lock().unwrap().len();
+                letter_index = (letter_index + 1i64) % data_length as i64;
+                *letter_index_arc.lock().unwrap()=letter_index;
+                
+                //TODO: Add brainfuck logic here
+                
+                thread::sleep(Duration::from_millis(100));
+            }
+        }));
+    }
+    pub fn stop_interpreter(&mut self) {
+        let mut timer_running = self.timer_running.lock().unwrap();
+        *timer_running = false;
+        if let Some(handle) = self.timer_thread_handle.take() {
+            handle.join().unwrap();
+        }
+    }
 }
 
 impl eframe::App for BrainfuckInterpreterInterface {
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-
             egui::menu::bar(ui, |ui| {
                 ui.menu_button("File", |ui| {
                     if ui.button("Quit").clicked() {
@@ -81,39 +119,79 @@ impl eframe::App for BrainfuckInterpreterInterface {
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            
-            ui.heading("Brainfuck code");
-            
-            let available_size = Vec2::new(ui.available_width(),0.0); // Remaining space in the panel
+            ui.horizontal(|ui| {
+                ui.heading("Brainfuck code");
+
+                if ui.button("Run").clicked() && !*self.timer_running.lock().unwrap() {
+                    self.start_interpreter();
+                };
+            });
+
+            let available_size = Vec2::new(ui.available_width(), 0.0);
             ui.add_sized(
                 available_size, // Use the available size
-                egui::TextEdit::multiline(&mut self.input_brainfuck)
+                egui::TextEdit::multiline(&mut *self.input_brainfuck.lock().unwrap())
                     .hint_text("Type brainfuck here..."),
             );
-            
+
             ui.add_space(10.0);
-            
+            let box_size = 50.0;
+            let highlight_color = Color32::RED;
+
             ui.horizontal(|ui| {
+                // Left side panel: Text and Input
                 ui.vertical(|ui| {
                     ui.heading("Output");
                     ui.add(
-                        egui::TextEdit::multiline(&mut self.output)
+                        egui::TextEdit::multiline(&mut *self.output.lock().unwrap())
                             .hint_text("This is output-only")
                             .interactive(false),
                     );
                     ui.add_space(10.0);
                     ui.heading("Input");
-                    ui.text_edit_multiline(&mut self.input_text);
+                    ui.text_edit_multiline(&mut *self.input_text.lock().unwrap());
                 });
-                
-                //boxes here
-                
+
+                // Right side panel: Dynamic boxes
+                ui.vertical(|ui| {
+                    // Adjust spacing between boxes
+                    ui.spacing_mut().item_spacing = egui::vec2(1.0, 1.0);
+
+                    ui.horizontal_wrapped(|ui| {
+                        for (i, value) in self.data.lock().unwrap().iter().enumerate() {
+                            // Allocate space for each box
+                            let (_, rect) = ui.allocate_space([box_size, box_size].into());
+
+                            // Highlight logic for the selected box
+                            let rect_color = if i == *self.letter_index.lock().unwrap() as usize {
+                                highlight_color
+                            } else {
+                                Color32::GRAY
+                            };
+
+                            // Draw the box
+                            ui.painter().rect_filled(rect, 0.0, rect_color);
+
+                            // Draw the value in the center of the box
+                            ui.painter().text(
+                                rect.center(),
+                                egui::Align2::CENTER_CENTER,
+                                format!("{}", value),
+                                egui::TextStyle::Body.resolve(ui.style()),
+                                Color32::WHITE,
+                            );
+                        }
+                    });
+                });
             });
-            
+
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 powered_by_egui_and_eframe(ui);
                 egui::warn_if_debug_build(ui);
             });
+
+            // Request a repaint to keep the animation going
+            ctx.request_repaint();
         });
     }
 
@@ -136,7 +214,8 @@ fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
         ui.label(".");
     });
     ui.hyperlink_to(
-                "Source code",
-                "https://github.com/Bombinisss/BrainfuckInterpreter/");
+        "Source code",
+        "https://github.com/Bombinisss/BrainfuckInterpreter/",
+    );
     ui.separator();
 }
